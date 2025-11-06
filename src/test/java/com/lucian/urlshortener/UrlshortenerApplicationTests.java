9b9da1d017c9feb75987030c7573aad45b9d56d9
lucian <locamos@gmail.com>
2025-11-06 18:32:38 +0000

    first commit

---
+++ src/test/java/com/lucian/urlshortener/UrlshortenerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.lucian.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortenerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
